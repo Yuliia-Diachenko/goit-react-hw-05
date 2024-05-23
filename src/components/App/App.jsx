33d9a34252bc06b046538bcf3a300209c86e388b
Yuliia Diachenko <zirkanew82@gmail.com>
2024-05-23 13:23:28 +0300

    Vercel+

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className={css.container}>
       <h1 className={css.font}>Do you like movies as much as I do?</h1>
+      <p>KEY:  2c735540e9638c9a06d5757398f5bb2f</p>
     </div>
   )
 }
