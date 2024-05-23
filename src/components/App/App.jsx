c71fa564cc644e1615815e27d3358315d5cb99da
Yuliia Diachenko <zirkanew82@gmail.com>
2024-05-23 13:01:16 +0300

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import css from "./App.module.css";
+
+function App() {
+
+  return (
+    <div className={css.container}>
+      <h1 className={css.font}>Do you like movies as much as I do?</h1>
+    </div>
+  )
+}
+
+export default App;
